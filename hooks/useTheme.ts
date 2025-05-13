c10961119176d18a36f54199ff340888eb31557f
aarthiofficial <aarthi.nbs@gmail.com>
2025-05-13 19:45:14 +0530

    Create useTheme.ts

---
+++ hooks/useTheme.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+export default function useTheme() {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const saved = localStorage.getItem('theme');
+    if (saved) {
+      setTheme(saved);
+      document.documentElement.classList.toggle('dark', saved === 'dark');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    document.documentElement.classList.toggle('dark', newTheme === 'dark');
+    localStorage.setItem('theme', newTheme);
+    setTheme(newTheme);
+  };
+
+  return { theme, toggleTheme };
+}
